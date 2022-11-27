62ed725a7aa8ed535d94591c791a96f49511088d
Cihat Girgin <cihatgirgin1@gmail.com>
2022-11-27 15:24:46 +0300

    Added entity framework packages

--- Razor.Inroduction.ViewComponentsAndPartialView.Web/Razor.Inroduction.ViewComponentsAndPartialView.Web/Models/Menu.cs
+++ Razor.Inroduction.ViewComponentsAndPartialView.Web/Razor.Inroduction.ViewComponentsAndPartialView.Web/Models/Menu.cs
@@ -6,7 +6,6 @@ namespace Razor.Inroduction.ViewComponentsAndPartialView.Web.Models
     {
         public Guid Id { get; set; }
         public string? Name { get; set; }
-        public string? Description { get; set; }
         public Guid ParentId { get; set; }
     }
 }

a9660d843e70737ecec8c05d8200f3e7d055a271
Cihat Girgin <cihatgirgin1@gmail.com>
2022-11-27 15:08:19 +0300

    Created dummy data creator

---
+++ Razor.Inroduction.ViewComponentsAndPartialView.Web/Razor.Inroduction.ViewComponentsAndPartialView.Web/Models/DummyMenuItems.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Razor.Inroduction.ViewComponentsAndPartialView.Web.Models
+{
+    public class DummyMenuItems
+    {
+        public List<Menu> MenuItems { get; set; }
+        public DummyMenuItems()
+        {
+            MenuItems = new();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var parent = new Menu
+                {
+                    Id = new Guid(),
+                    Name = $"Category {i}",
+                    ParentId = Guid.Empty
+                };
+
+                MenuItems.Add(parent);
+
+                for (int j = 0; j < 5; j++)
+                {
+                    var child = new Menu
+                    {
+                        Id = new Guid(),
+                        Name = $"Category {i}-{j}",
+                        ParentId = parent.Id
+                    };
+
+                    MenuItems.Add(child);
+
+                    for (int y = 0; y < 5; y++)
+                    {
+                        var grandChild = new Menu
+                        {
+                            Id = new Guid(),
+                            Name = $"Category {i}-{j}-{y}",
+                            ParentId = child.Id
+                        };
+
+                        MenuItems.Add(grandChild);
+                    }
+                }
+            }
+
+        }
+
+    }
+}
---
+++ Razor.Inroduction.ViewComponentsAndPartialView.Web/Razor.Inroduction.ViewComponentsAndPartialView.Web/Models/Menu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Razor.Inroduction.ViewComponentsAndPartialView.Web.Models
+{
+    public class Menu
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public Guid ParentId { get; set; }
+    }
+}
